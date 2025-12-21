da5336ad95bdf1abc8d9ffdce7313855eee920d9
Balasubramanikarthi <karthi.selvisubramani@gmail.com>
2025-12-21 22:03:37 +0530

    Add project files.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Students_Login.Data;
+using Students_Login.Models;
+using System.Linq;
+
+namespace SimpleLoginApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public AccountController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(User user)
+        {
+            var result = _context.Users
+                .FirstOrDefault(u => u.Username == user.Username && u.Password == user.Password);
+
+            if (result != null)
+            {
+                return RedirectToAction("Welcome");
+            }
+
+            ViewBag.Message = "Invalid Username or Password";
+            return View();
+        }
+
+        public IActionResult Welcome()
+        {
+            return View();
+        }
+    }
+}
---
+++ StData/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Students_Login.Models;
+
+namespace Students_Login.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
