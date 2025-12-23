30d3ed66d31feb6539fd53d1aee8ba59e1e8657d
KarthiBalasubramani <karthikeyan.selvisubramani@gmail.com>
2025-12-23 15:22:10 +0530

    Students_Login Program MSSQL to Change MYSQL .

--- Program.cs
+++ Program.cs
@@ -5,8 +5,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseMySql(
+        connectionString,
+        ServerVersion.AutoDetect(connectionString)
+    ));
 
 var app = builder.Build();
 
